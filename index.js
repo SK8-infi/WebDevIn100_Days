9e2f4a60014d98e8136de95b595f8ad71601bf7d
SK8-infi <shivansh.katiyar1712@gmail.com>
2025-07-30 03:59:23 +0530

    Add css math duel

--- index.js
+++ index.js
@@ -139,7 +139,8 @@ function fillTable() {
         ["Day 36","World Clock","public/World_Clock/index.html"],
         ["Day36","World Clock","public/World_Clock/index.html"],
         ["Day 40", "Space War Game" ,"./public/Space-War-Game/index.html"],
-        ["Day 44", "Notes App" ,"./public/Day-36_Notes_App/index.html"],];
+        ["Day 44", "Notes App" ,"./public/Day-36_Notes_App/index.html"],
+        ["Day 45", "CSS Math Duel" ,"./public/Day-45_CSS_Math_Duel/index.html"],];
 
 
     const tbody = document.getElementById('tableBody');
